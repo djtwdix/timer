0a3277a29d7f1a87600c3089a6c657a7236acd38
djtwdix <djtwd.ix@icloud.com>
2021-02-09 16:06:03 -0400

    Changed 'say beep' to system sound (works in native terminal)

--- timer1.js
+++ timer1.js
@@ -3,9 +3,9 @@ const args = process.argv.slice(2);
 const timer = args => {
   for (const time of args) {
     if (time > 0 && typeof Number(time) === "number") {
-      setTimeout(() => process.stdout.write('say beep '), time * 1000)
+      setTimeout(() => process.stdout.write('\x07'), time * 1000)
     }
   }
 }
 
-timer(args)
+timer(args);
